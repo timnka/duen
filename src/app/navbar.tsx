1c6ac6531bf0a694f3cd0d7d12b7c83130cb7bd0
takumi <takumi.imanaka@gmail.com>
2023-08-01 01:11:10 -0700

    bg and typewriter effect in place

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,17 +2,19 @@ import logo from './duen.png'
 import Image from 'next/image'
 
 export default function NavBar() {
-    return (
-      <div className="w-full bg-white flex justify-center drop-shadow-md">
-        <ul className="h-full w-full flex justify-around items-center text-lg">
-          <li>About</li>
-          <li>Officers</li>
-          <Image src={logo} alt="duen logo" height={70} className="py-3" />
-          <li>Join</li>
-          <li>Contact</li>
-        </ul>
-        
-      </div>
-    )
-  }
-  
+ /* solid white header       blurry backdrop: bg-transparent backdrop-blur-lg
+ <div className="w-11/12 bg-white fixed rounded-b-lg flex justify-center drop-shadow-md ">
+    <ul className="h-full w-full flex justify-around items-center text-lg text-black">
+ */
+  return (
+    <div className="w-full bg-white fixed z-10 flex justify-center drop-shadow-md ">
+    <ul className="h-full w-full flex justify-around items-center text-lg text-black">
+        <li>About</li>
+        <li>Officers</li>
+        <Image src={logo} alt="duen logo" height={55} className="py-2" />
+        <li>Join</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+"use client"
 import Image from 'next/image'
 import logo from './duen.png'
 import NavBar from './navbar'
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Home() {
+
   return (
-    <div>
-      <NavBar></NavBar>
-    </div>
+    <>
+      <div className="flex justify-center">
+        <NavBar></NavBar>
+      </div>
+
+
+      <div className="w-full h-screen bg-cover bg-top bg-[url('./grads1.jpg')] flex  items-center text-[#d787ff] text-6xl">
+        <div className="w-6/12 flex justify-end">
+          <p className="text-white pr-3">We are... </p>
+        </div>
+        <div>
+          <Typewriter
+            words={['lifelong friends.', 'adventurers.', 'engineers!']}
+            loop={false}
+            cursor
+            cursorStyle='|'
+            typeSpeed={120}
+            deleteSpeed={120}
+            delaySpeed={3000}/>
+        </div>
+
+
+      </div>
+
+      <div className="h-96"></div>
+    </>
   )
 }
