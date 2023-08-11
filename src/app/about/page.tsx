d0c9e3dbc65251c4d0fbac61909a09cd04b7740b
Takumi Imanaka <takumi.imanaka@gmail.com>
2023-08-11 02:24:12 -0700

    minor spacing adjustments to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -60,18 +60,18 @@ export default function About() {
         <div className="flex flex-col items-center bg-white">
             <Banner word="ABOUT"></Banner>
 
-            <div className="w-full min-h-fit flex flex-col items-center px-16 md:px-32 lg:px-64 py-20">
-                <p className="font-teko text-5xl py-7 text-purple-800">OUR MISSION</p>
+            <div className="w-full min-h-fit flex flex-col items-center px-16 md:px-32 lg:px-64">
+                <p className="font-teko text-5xl py-16 text-purple-800">OUR MISSION</p>
 
                 {/* Mission Photo */}
-                <div className="w-96 min-h-fit py-7">
+                <div className="w-96 min-h-fit">
                     <div className="h-full flex justify-center items-center object-scale-down overflow-hidden relative group rounded-lg">
                         <p className="absolute text-2xl text-white z-10 invisible group-hover:visible">Cool guy with pit vipers on his way to school</p>
                         <Image src={pits} alt="duen group photo" objectFit={'contain'} className="group-hover:brightness-50 group-hover:scale-110 transition duration-500 cursor-pointer z-0"></Image>
                     </div>
                 </div>
 
-                <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="py-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
 
             <div className="flex flex-col items-center pb-10">
