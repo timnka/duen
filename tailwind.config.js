ed3970fe1af8c5013fd29656bce16d846dd331d9
Takumi Imanaka <takumi.imanaka@gmail.com>
2023-08-30 22:59:02 -0700

    updated image file type case sensitivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'landing':"url('/landing-content/grads1.jpg')"
+        'landing':"url('/landing-content/grads1.JPG')"
       },
     },
   },
