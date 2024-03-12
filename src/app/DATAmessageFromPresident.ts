0ce5d44f44eeda6945e75095760dfd86d402d92c
jessicatrans <jgtrans@ucdavis.edu>
2024-03-11 22:29:26 -0700

    update message from president photo

--- src/app/DATAmessageFromPresident.ts
+++ src/app/DATAmessageFromPresident.ts
@@ -27,5 +27,5 @@ export const messageFromPresident = {
     message: 'Hello everyone! Davis Undergraduate Engineering Network (DUEN) is a club at UC Davis for those looking to connect with students interested in Engineering and Computer Science, interested in connecting with their peers! We host general member events, open to anyone, pertaining to our core values of Professional Development, Camaraderie, and Community Service. Through DUEN, I’ve made lifelong friends who have truly shaped my undergraduate experience. I take pride in representing this organization and am excited to expand our community further! I look forward to seeing everyone at events this term! ',
     presidentName: 'Semra Khan',
     presidentCohort: 'Founding Cohort',
-    imagePath: '/officers/Khan_Semra.jpg'
+    imagePath: '/officers/Khan_Semra.JPG'
 }
