1b89cdf5afb9ae8763c45a8d008271fe9f1e4423
jessicatrans <jgtrans@ucdavis.edu>
2024-03-31 00:54:53 -0700

    Fix image wording

--- src/app/officers/DATAinternal.ts
+++ src/app/officers/DATAinternal.ts
@@ -99,7 +99,7 @@ export const internal = [
     cohort: 'Fall 2022 Cohort',
     position: 'Internal Affairs',
     linkedin: 'https://www.linkedin.com/in/abubakr-hadwan-404aa918a/',
-    image: '/officers/hadwan_abubakr.JPG',
+    image: '/officers/Hadwan_Abubakr.JPG',
   },
   {
     name: 'Daphne Lee',
